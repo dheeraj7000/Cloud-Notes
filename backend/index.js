e418f622183da5cf32239743d78954f9e64e958c
ANURAG AGARWAL <anuragagarwal203@gmail.com>
2022-07-17 02:17:48 +0530

    msg

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const connectTOMongo = require('./db');
+const express = require('express')
+connectTOMongo();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Anurag!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
